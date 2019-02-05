d59c29d9509eedf8a19536ec87c2f269bfa31c35
galvarez71 <47159893+galvarez71@users.noreply.github.com>
2019-02-05 13:56:57 -0500

    Add files via upload

---
+++ Homework/Character-Interactive/sketch.js
@@ -0,0 +1,208 @@
+//Global Variables
+var backgroundColor = "blue"; //Strings
+var colormimi = "green";
+var bow = "red";
+var strokecolor = "black";
+var textcolor = "yellow";
+var mimiDress = "yellow";
+var bottomText = "white";
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+  createCanvas(600,700);
+  background(backgroundColor);
+
+  // left ponytail - first part
+  strokeWeight(4);
+  stroke("black");
+  fill("green");
+  rect(100,100,90,90);
+
+  // left ponytail - second part
+  fill(colormimi);
+  rect(90,160,50,50);
+
+  // left ponytail - third part
+  fill(colormimi);
+  rect(130,190,30,40);
+
+  //right ponytail - first part
+  fill(colormimi);
+  rect(355,100,90,90);
+
+  //right ponytail - second part
+  fill(colormimi);
+  rect(430,160,50,50);
+
+  // right ponytail - third part
+  fill(colormimi);
+  rect(410,190,30,40);
+
+  //body - torso
+  strokeWeight(7);
+  stroke("black");
+  fill(mimiDress);
+  rect(235,320,80,90);
+
+  strokeWeight(1);
+
+  //red bow - left part
+  strokeWeight(7);
+  stroke(strokecolor);
+  fill(bow);
+  triangle(235,340,235,385,270,360);
+
+  strokeWeight(1);
+
+  //red bow - right part
+  strokeWeight(7);
+  stroke(strokecolor);
+  fill(bow);
+  triangle(315,340,315,385,270,360);
+
+  strokeWeight(1);
+
+  //left arm
+  strokeWeight(7);
+  stroke("black");
+  line(235,345,90,375);
+
+  strokeWeight(1);
+
+  //right arm
+  strokeWeight(7);
+  stroke("black");
+  line(315,345,460,375);
+
+  strokeWeight(1);
+
+  //head
+  strokeWeight(4);
+  stroke("black");
+  fill("green");
+  rect(180,165,190,180);
+
+  strokeWeight(1);
+
+  //left leg
+  strokeWeight(7);
+  stroke("black");
+  line(235,450,240,600);
+
+  strokeWeight(1);
+
+  //right leg
+  strokeWeight(7);
+  stroke("black");
+  line(305,450,305,600);
+
+  strokeWeight(1);
+
+  //first eye
+  fill("black");
+  ellipse(235,250,15,25);
+
+  //second eye
+  fill("black");
+  ellipse(310,250,15,25);
+
+  //left cheek
+  strokeWeight(0);
+  fill("pink");
+  ellipse(220,290,35,15);
+
+  //right cheek
+  strokeWeight(0);
+  fill("pink");
+  ellipse(330,290,35,15);
+
+  //skirt
+  strokeWeight(7);
+  stroke("black");
+  fill(mimiDress);
+  arc(275, 410, 230, 230, 0, PI);
+
+  strokeWeight(1);
+
+  //mouth
+  strokeWeight(0);
+  fill("red");
+  arc(275, 305, 40, 30, 0, PI);
+
+  //eyelash right
+  fill("black");
+  triangle(310, 250, 315, 240, 330, 235);
+
+  //eyelash left
+  fill("black");
+  triangle(235, 240, 235, 250, 215, 235);
+
+  //belt
+  strokeWeight(7);
+  stroke("black");
+  line(160,410,390,410);
+
+  //belly
+  strokeWeight(0);
+  fill(mimiDress);
+  rect(239,405,73,60);
+
+  //left pigtail clip
+  strokeWeight(5);
+  fill(mimiDress);
+  ellipse(180,165,30,30);
+
+  //right pigtail clip
+  strokeWeight(5);
+  fill(mimiDress);
+  ellipse(365,165,30,30);
+
+
+  strokeWeight(1);
+
+  textSize(28);
+  fill(textcolor);
+  text('Mimi from Super Paper Mario', 100, 30);
+  textSize(16);
+  text('By Grettel Alvarez     CIM540-640      Professor Rodriguez',100,50);
+
+  textSize(14);
+  fill(bottomText);
+  text('Click your mouse to make the background disappear!', 100, 630);
+  textSize(14);
+  fill(bottomText);
+  text('Press different keys to change her dress color! *Hint* Try pressing R, O, P and L!', 60, 655);
+
+}
+
+  // end of draw loop
+
+  function mousePressed(){
+    backgroundColor = "white";
+    bottomText = "black";
+    textcolor = "black";
+  }
+
+  function mouseReleased(){
+    backgroundColor = "blue";
+    bottomText = "white";
+    textcolor = "yellow";
+  }
+
+  function keyPressed(){
+    if(key == 'p'){
+      mimiDress = "pink";
+    }else if (key == 'r'){
+      mimiDress = "red";
+    }else if (key == 'o'){
+      mimiDress = "orange";
+    }else if (key == 'l'){
+      mimiDress = "purple";
+    }else{
+      mimiDress = "white";
+    }
+  }
